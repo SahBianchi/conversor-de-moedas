c2bd636b7a49864143e38432682e4c06c96538f1
inaralmeida <inara.ralmeida@gmail.com>
2023-02-07 18:34:55 -0300

    fix: sintaxe function conversor

--- conversorDeMoedas.js
+++ conversorDeMoedas.js
@@ -1,79 +1,64 @@
 // Moedas referente ao lado esquerdo (valor a ser convertido)
-const opcaoRealLE = document.getElementById('moedaRealLE').value
-const opcaoEuroLE = document.getElementById('moedaEuroLE').value
-const opcaoDolarLE = document.getElementById('moedaDolarLE').value
 const txtMoedaLE = document.getElementById('txtMoedaLE')
+const moedaInicial = document.getElementById('opcaoMoedaLE')
 
 // Moedas referente ao lado direito (valor convertido)
-const opcaoRealLD = document.getElementById('moedaRealLD').value
-const opcaoEuroLD = document.getElementById('moedaEuroLD').value
-const opcaoDolarLD = document.getElementById('moedaDolarLD').value
 const txtMoedaLD = document.getElementById('txtMoedaLD')
+const moedaFinal = document.getElementById('opcaoMoedaLD')
 
 const valorConvertido = document.getElementById('valorConvertido')
 
+const btnConverter =  document.getElementById('botaoConverter')
+
+const valorMoedas = {
+    real:{
+        valor: {
+            dolar: 0.19,
+            euro: 0.18,
+            real: 1
+        },
+        moeda: 'R$'
+    },
+    euro:{
+        valor: {
+            dolar: 1.07,
+            euro: 1,
+            real: 5.53
+        },
+        moeda: 'EUR'
+    },
+    dolar:{
+        valor: {
+            dolar: 1,
+            euro: 0.93,
+            real: 5.15
+        },
+        moeda: 'USD'
+    },
+}
+
+btnConverter.addEventListener('click', (e)=> {
+    converter()
+    e.preventDefault()
+})
+
 function converter(){
-    const moedaLE = document.getElementById('opcaoMoedaLE');
-	const opcaoLE = moedaLE.options[moedaLE.selectedIndex];
+    //Pega o valor selecionado nos select
+    const tipoMoedaInicial = moedaInicial.value
+    const tipoMoedaFinal = moedaFinal.value
 
-    const moedaLD = document.getElementById('opcaoMoedaLD');
-	const opcaoLD = moedaLD.options[moedaLD.selectedIndex];
+    //Pega o valor a ser convertido
+    const valorConverter = document.getElementById("valorConverter").value;
+    
+    //Altera o tipo da moeda no HTML, Ex: EUR, R$ e USD
+    txtMoedaLE.textContent = valorMoedas[tipoMoedaInicial].moeda
+    txtMoedaLD.textContent = valorMoedas[tipoMoedaFinal].moeda
 
-    if(opcaoLE.value == opcaoDolarLE && opcaoLD.value == opcaoRealLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">USD</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">R$</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorDolarEmReal = valorConverter * 5.15
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorDolarEmReal.toFixed(2)}</p>` 
-    }else if(opcaoLE.value == opcaoDolarLE && opcaoLD.value == opcaoEuroLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">USD</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">EUR</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorDolarEmEuro = valorConverter * 0.93
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorDolarEmEuro.toFixed(2)}</p>` 
-    }else if(opcaoLE.value == opcaoDolarLE && opcaoLD.value == opcaoDolarLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">USD</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">USD</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorDolarEmDolar = valorConverter * 1
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorDolarEmDolar.toFixed(2)}</p>` 
-    }else if(opcaoLE.value == opcaoEuroLE && opcaoLD.value == opcaoRealLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">EUR</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">R$</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorEuroEmReal = valorConverter * 5.53
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorEuroEmReal.toFixed(2)}</p>`
-    }else if(opcaoLE.value == opcaoEuroLE && opcaoLD.value == opcaoDolarLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">EUR</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">USD</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorEuroEmDolar = valorConverter * 1.07
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorEuroEmDolar.toFixed(2)}</p>`
-    }else if(opcaoLE.value == opcaoEuroLE && opcaoLD.value == opcaoEuroLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">EUR</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">EUR</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorEuroEmEuro = valorConverter * 1
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorEuroEmEuro.toFixed(2)}</p>`
-    }else if(opcaoLE.value == opcaoRealLE && opcaoLD.value == opcaoDolarLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">R$</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">UDS</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorRealEmDolar = valorConverter * 0.19
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorRealEmDolar.toFixed(2)}</p>`
-    }else if(opcaoLE.value == opcaoRealLE && opcaoLD.value == opcaoEuroLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">R$</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">EUR</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorRealEmEuro = valorConverter * 0.18
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorRealEmEuro.toFixed(2)}</p>`
-    }else if(opcaoLE.value == opcaoRealLE && opcaoLD.value == opcaoRealLD){
-        txtMoedaLE.innerHTML = `<p id="txtMoedaLE">R$</p>`
-        txtMoedaLD.innerHTML = `<p id="txtMoedaLD">R$</p>`
-        const valorConverter = document.getElementById("valorConverter").value;
-        const valorRealEmReal = valorConverter * 1
-        valorConvertido.innerHTML = `<p class="inputLD" id="valorConvertido">${valorRealEmReal.toFixed(2)}</p>`
-    }
+    //Acessa o valor de conversão baseado nos tipos de moedas passadas nos selects e multiplica pelo valor digitado no input 
+    const valorFinal = valorConverter * valorMoedas[tipoMoedaInicial].valor[tipoMoedaFinal]
+
+    //Mostra o valor convertido na tela
+    valorConvertido.textContent = valorFinal.toFixed(2)
 }
 
 converter()
